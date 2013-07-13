2a68fbd6c0872924c5d5cb285950595aba3ac50a
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-07-13 15:21:03 -0700

    clean version; add newrelic gem

--- app/helpers/drinks_helper.rb
+++ app/helpers/drinks_helper.rb
@@ -3,14 +3,14 @@ module DrinksHelper
   def random_suggest
     suggestions = [
       "You should whip up a...",
-      "How 'bout a fucking...",
+      "How 'bout a...",
       "For instant oblivion, knock back a...",
-      "Thirsty?  Fuckin' make a...",
-      "Wet your fuckin' whistle with a...",
+      "Thirsty?  Make a...",
+      "Wet your whistle with a...",
       "You aren't too good for a...",
-      "Fuck off, make a...",
-      "If I were you, I'd make a fucking...",
-      "Have a fucking...",
+      "Hey, make a...",
+      "If I were you, I'd make a...",
+      "Have a...",
       "You look like you could use a...",
       "Life would be better with a..."
     ]
@@ -20,20 +20,20 @@ module DrinksHelper
 
   def refresh_text
     refusals = [
-      "Fuck off",
+      "Buzz off",
       "That sounds gross",
       "Nasty",
       "I'd rather puke",
-      "Thanks but no fucking way",
+      "Thanks but no way",
       "Eeew!",
       "Ugh...",
       "Rat poison sounds more palatable",
       "Gag me",
-      "No fucking thank you",
+      "No thank you",
       "Sounds awful",
       "Barf",
       "Not on your life",
-      "I don't fucking think so"
+      "I don't think so"
     ]
 
     refusals.sample
